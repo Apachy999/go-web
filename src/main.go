3e20969b3f297377769bd4fa7dfad9f073e191bb
KATSIARYNA BURMEISTSER <apachy999@gmail.com>
2022-07-06 14:30:04 +0300

    add style

--- src/main.go
+++ src/main.go
@@ -1,25 +1,39 @@
 package main
-import ("fmt"
+import (
         "net/http"
         "html/template")
 
 func index(w http.ResponseWriter, r *http.Request){
-  tmpl, _ := template.ParseFiles("views/index.html")
+  tmpl, _ := template.ParseFiles("views/index")
   tmpl.Execute(w, "hello")
+
 }
 
-func contacts(w http.ResponseWriter, r *http.Request){
-  fmt.Fprintf(w, "contacts page")
+func products(w http.ResponseWriter, r *http.Request){
+  tmpl, _ := template.ParseFiles("views/products")
+  tmpl.Execute(w, "hello")
+
   }
 
+func contacts(w http.ResponseWriter, r *http.Request){
+  tmpl, _ := template.ParseFiles("views/contacts")
+  tmpl.Execute(w, "hello")
+
+  }
 
 func handleRequest(){
 //  fmt.Println("Go Rulit!")
   http.HandleFunc("/", index)
+  http.HandleFunc("/products", products)
   http.HandleFunc("/contacts", contacts)
   http.ListenAndServe(":8080",nil)
 }
 
 func main(){
+http.Handle("/static/",
+  http.StripPrefix("/static/",
+    http.FileServer(http.Dir("static"))))
+    
   handleRequest()
+
 }
